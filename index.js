116e71f4c5df9a541941d2581d4988ee858eddfd
Manuel Pissanos <mpissano@gmail.com>
2018-08-28 00:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
+ var grandTotal = `${total()}`
   if (!cardNumber) {
       return "Sorry, we don't have a credit card on file for you.";
     } else {
